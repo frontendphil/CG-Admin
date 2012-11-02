e9a6d7cf4df5420f36cbfc0f49159c0c17bc15b5
Philipp Giese <giese.philipp+git@gmail.com>
2012-11-02 16:44:18 +0100

    added advanced navigatino features
    - breadcrumps now show a much more detailed view of the position on the
    webpage
    - links can be automagically set using rel="abort" or rel="forward"
    attribute (may merge these)

--- admin/static/js/general.js
+++ admin/static/js/general.js
@@ -1,6 +1,30 @@
 (function($) {
 
     $(document).ready(function() {
+        var updateTarget = function(link, attr) {
+            var url = window.location.href;
+            var parts = url.split("#");
+
+            var remainder = parts.slice(1);
+            var target = link.attr(attr);
+
+            if(target) {
+                target = target + "#";
+            } else {
+                target = "";
+            }
+
+            link.attr(attr, target + remainder.join("#"));
+        };
+
+        $("a[rel=abort], a[rel=forward]").each(function(index, a) {
+            updateTarget($(a), "href");
+        });
+
+        $("button[rel=forward], button[rel=abort]").each(function(index, btn){
+            updateTarget($(btn), "target");
+        });
+
         $("button").each(function(index, btn) {
             btn = $(btn);
 
