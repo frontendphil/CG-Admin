fe999084c869476945a9691648b8c48afcbb31af
Philipp Giese <giese.philipp+git@gmail.com>
2013-01-31 15:38:37 +0100

    fixed a bug that caused a 500 if a search for a patient or doctor contained a dash

--- admin/urls/search.py
+++ admin/urls/search.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns("admin.views",
     url(r'^patient/$', "patient.search", name="search_patient"),
     url(r'^patient/page/(?P<page>\d+)/$', 'patient.list', name='list_patients_page'),
-    url(r'^patient/query/(?P<query>(\w+|\s)+)/page/(?P<page>\d+)/$', 'patient.list', name='list_patients'),
+    url(r'^patient/query/(?P<query>(\w+|\s|-)+)/page/(?P<page>\d+)/$', 'patient.list', name='list_patients'),
 
     url(r'^doc/$', "docs.search", name="search_docs"),
     url(r'^doc/page/(?P<page>\d+)/$', 'docs.list', name='list_docs_page'),
-    url(r'^doc/query/(?P<query>(\w+|\s)+)/page/(?P<page>\d+)/$', 'docs.list', name='list_docs'),
+    url(r'^doc/query/(?P<query>(\w+|\s|-)+)/page/(?P<page>\d+)/$', 'docs.list', name='list_docs'),
 )
